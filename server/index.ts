1e4ce5b2543c1803bf342b64dc2f6c9b90abe612
timogal <leesj.isone@gmail.com>
2018-05-01 21:45:49 +0800

    pm2 process.json

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const app = express();
 
 setup(app);
 
-const port: number = parseInt(process.env.PORT || '8080', 10);
+const port: number = parseInt(process.env.PORT || '9000', 10);
 
 app.listen(8080, function (err: Error) {
   if (err) {
