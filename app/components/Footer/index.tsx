4a06b3e4e700efa0d9c93ea9ead4695a74014a81
Timogal <leesj.isone@gmail.com>
2018-08-07 21:15:14 +0800

    detail page

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -11,7 +11,7 @@ class Footer extends React.Component<any, any> {
         <Main className={styles.content}>
           <ul className={styles.info}>
             <li>粤ICP备18043338号-1</li>
-            <li>Copyright &copy; Timogal 2016-2018</li>
+            <li>Copyright &copy;  2016-2018 Timogal</li>
             <li><a target="_blank" href="https://github.com/timogal/react-ts-blog">网站源码</a></li>
             <li><a href="http://www.cnzz.com/stat/website.php?web_id=1274226487" target="_blank" title="站长统计">站长统计</a>
             </li>
--- app/containers/DetailPage/index.tsx
+++ app/containers/DetailPage/index.tsx
@@ -1,12 +1,41 @@
 import * as React from 'react';
-import Main from 'components/Main';
+import { Breadcrumb, Icon } from 'antd';
+import { Link } from 'react-router-dom';
+
+import detail from './mock';
+
+import styles from './detail.scss';
+
+const BreadItem = Breadcrumb.Item;
 
 class DetailPage extends React.Component<any, any> {
   render(): JSX.Element {
     return (
-      <Main>
-        Detail Page!
-      </Main>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>{detail.title}</h1>
+        <ul className={styles.meta}>
+          <li>
+            <Icon type="calendar" className={styles.icon} /> {detail.time}
+          </li>
+          <li>
+            <Icon type="eye" className={styles.icon} /> 1200
+          </li>
+          <li>
+            <Icon type="folder" className={styles.icon} />
+            分类于：
+            <Breadcrumb className={styles.category} separator="|">
+              {
+                detail.categories.map(item => (
+                  <BreadItem key={item.id}>
+                    <Link to={`/category/${item.id}`}>{item.name}</Link>
+                  </BreadItem>
+                ))
+              }
+            </Breadcrumb>
+          </li>
+        </ul>
+        <article className={styles.article} dangerouslySetInnerHTML={{ __html: detail.content }} />
+      </div>
     );
   }
 }
